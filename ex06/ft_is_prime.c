8051f27b4d2be00abdeef2f349cf783ce89ef824
Emile Fournier <emfourni@bess-f4r4s3.clusters.42paris.fr>
2023-07-05 16:55:08 +0200

    third push

--- ex06/ft_is_prime.c
+++ ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: emfourni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:05:18 by emfourni          #+#    #+#             */
-/*   Updated: 2023/07/05 16:16:21 by emfourni         ###   ########.fr       */
+/*   Updated: 2023/07/05 16:54:25 by emfourni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,4 @@ int	ft_is_prime(int nb)
 		i++;
 	}
 	return (1);
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		printf("%i\n", ft_is_prime(atoi(argv[1])));
-	return (0);
 }
