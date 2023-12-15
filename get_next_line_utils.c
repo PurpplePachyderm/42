e8ae4946e57e93ab22806d9353f7dd29bdaba230
PurpplePachyderm <73134820+PurpplePachyderm@users.noreply.github.com>
2023-12-15 13:43:41 +0100

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emilefournier <emilefournier@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 12:21:24 by emilefourni       #+#    #+#             */
+/*   Updated: 2023/12/12 13:57:14 by emilefourni      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int found_new_line(t_list *stash)
+{
+    int     i;
+    t_list *current;
+
+    if (!stash)
+        return (0);
+    current = ft_lst_get_last(stash);
+    i = 0;
+    while (current->content[i])
+    {
+        if (current->content[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+t_list  *ft_lst_get_last(t_list *stash)
+{
+    t_list *current;
+
+    current = stash;
+    while (current && current->next)
+        current = current->next;
+    return (current);
+}
+
+void    generate_line(t_list *stash, char **line)
+{
+    int i;
+    int len;
+
+    len = 0;
+    while (stash)
+    {
+        i = 0;
+        while (stash->content[i])
+        {
+            if (stash->content[i] == '\n')
+            {
+                len++;
+                break;
+            }
+            len++;
+            i++;
+        }
+        stash = stash->next;
+    }
+    *line = malloc(sizeof(char) * (len + 1));
+    if (!line)
+        return;
+}
+
+void    free_stash(t_list *stash)
+{
+    t_list *current;
+    t_list *next;
+
+    current = stash;
+    while (current)
+    {
+        free(current->content);
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+size_t ft_strlen(const char *str)
+{
+    size_t  i;
+
+    i = 0;
+    if (!str)
+        return (0);
+    while (str[i])
+        i++;
+    return (i);
+}
